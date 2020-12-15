6e0c270fdcb33680a63e89cb8f4d64792b16f439
patricklapgar <patricklapgardeveloper@gmail.com>
2020-12-15 14:14:58 -0600

    Created function to validate user guesses

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -8,6 +8,30 @@ def find_next_empty(puzzle):
 
     return None, None 
 
+def is_valid(puzzle, guess, row, col):
+    # Determines if the guess is valid or not
+    # Returns True if valid, else returns False
+    row_values = puzzle[row]
+    # If the user's guess is already a number in that row, return False
+    if guess in row_values:
+        return False
+
+    col_values = [puzzle[i][col] for i in range(9)]
+    if guess in col_values:
+        return False
+
+    # Find where the individual 3x3 square matrix begins
+    # iterate over the 3 values in each row/column
+    starting_row = (row // 3) * 3
+    starting_column = (col // 3) * 3
+
+    for r in range(starting_row, starting_row + 3):
+        for c in range(starting_column, starting_column + 3):
+            if puzzle[r][c] == guess:
+                return False
+
+    return True # Returns True if all validation checks pass
+
 def solve_sudoku(puzzle):
     # Use backtracking method to solve sudoku puzzle
     # Returns True if a solution exists or False if it doesn't
